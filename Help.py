fcc4b5ec84fde16dfb9314926c9cde82d10293d8
Daniel Love <danlove99@hotmail.co.uk>
2020-03-28 22:52:24 +0000

    Create Help.py

---
+++ Help.py
@@ -0,0 +1,9 @@
+help = '''
+exit = exit
+newTree = creates a new tree
+append = appends a new node to a tree
+traverse = choose a way to traverse and print a tree
+reverse = reverse a tree
+size = get the size of a tree
+height = get the height of a 
+'''
